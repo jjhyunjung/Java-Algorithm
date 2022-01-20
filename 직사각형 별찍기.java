d12c4c76bc9d6763d63594475c1d9edcd5e5c084
Jihyun Jung <97021735+jjhyunjung@users.noreply.github.com>
2022-01-20 21:49:05 +0900

    Update 직사각형 별찍기.java

--- 직사각형 별찍기.java
+++ 직사각형 별찍기.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		int n = sc.nextInt();
+		int m = sc.nextInt();
 
-		for (int i = 0; i < b; i++) {
-			for (int j = 0; j < a; j++) {
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
 				System.out.print("*");
 			}
 
