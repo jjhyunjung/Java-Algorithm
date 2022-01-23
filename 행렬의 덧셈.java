73cbbfe7021504906f4d0b5ffb52e0fbcfbe314d
Jihyun Jung <97021735+jjhyunjung@users.noreply.github.com>
2022-01-23 16:06:14 +0900

    Create 행렬의 덧셈.java

---
+++ 행렬의 덧셈.java
@@ -0,0 +1,16 @@
+class Solution {
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+
+		int[][] arr = new int[arr1.length][arr1[0].length];
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr1[0].length; j++) {
+				arr[i][j] = arr1[i][j] + arr2[i][j];
+			}
+		}
+		return arr;
+	}
+}
+
+//2차원 배열의 length는 1차원 배열과 다름
+//배열변수.length : 행의 개수
+//배열변수[인덱스].length : 각 행이 가지고 있는 열의 개수
